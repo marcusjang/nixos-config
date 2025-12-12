b5f4089b6a4787eafb1c463b2476ca6e8f9c17ab
Marcus Jang <marcus@melange.works>
2025-12-13 01:05:12 +0900

    Up deno to 2.6.0

--- overlays/deno.nix
+++ overlays/deno.nix
@@ -2,12 +2,12 @@
 final: prev: {
 	deno = prev.deno.overrideAttrs (old: rec {
 		pname = old.pname;
-		version = "2.5.2";
+		version = "2.6.0";
 		name = "${old.pname}-${version}";
 
 		src = final.pkgs.fetchurl {
 			url = "https://github.com/denoland/deno/releases/download/v${version}/deno-x86_64-unknown-linux-gnu.zip";
-			hash = "sha256-Ug/UvC0YtQtqwuqLfQ9/ajVYjkEnVKCB6Mav1LFSZMQ=";
+			hash = "sha256-UG/o2qIR5AFalPsXAugmNnVrw86yzDTU/j7zG4KjaEQ=";
 		};
 
 		nativeBuildInputs = with final.pkgs; [

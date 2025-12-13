25e6d251dee5c12264b35503e0a6ae2ad761bac4
Marcus Jang <marcus@melange.works>
2025-12-13 16:12:48 +0900

    Add gulim font

---
+++ pkgs/gulim.nix
@@ -0,0 +1,31 @@
+{
+	lib,
+	stdenvNoCC,
+	fetchurl,
+}:
+
+stdenvNoCC.mkDerivation rec {
+	pname = "gulim";
+	version = "1.0.0";
+
+	src = fetchurl {
+		url = "https://github.com/googlefonts/gulim/raw/refs/heads/main/fonts/ttf/bitmap/${pname}-Regular.ttf";
+		hash = "sha256-61wDircP2YVrundnk/XbIU3HyipVWfczks8E7CsVGUY=";
+	};
+
+	unpackPhase = ":";
+
+	installPhase = ''
+		runHook preInstall
+		install -m444 -Dt $out/share/fonts $src
+		runHook postInstall
+	'';
+
+	meta = {
+		description = "Gulim Korean Font";
+		homepage = "https://github.com/googlefonts/gulim";
+		license = lib.licenses.ofl;
+	};
+}
+
+

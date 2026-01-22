bfcfd63d06f65db897be159e1f95f25e43931b92
Marcus Jang <marcus@melange.works>
2026-01-23 03:23:03 +0900

    Change fileSystem mounts to systemd mounts & add WebDAV mount

--- modules/mounts.nix
+++ modules/mounts.nix
@@ -1,29 +1,108 @@
-{ pkgs, ... }: let
+{ config, pkgs, ... }: let
 	automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-	credentials = "credentials=/mnt/.smbcred";
 	owner = "uid=1000,gid=100";
 in {
-	environment.systemPackages = [ pkgs.cifs-utils ];
-	fileSystems = {
-		"/mnt/drive0" = {
-			device = "//10.0.10.2/drive0";
-			fsType = "cifs";
-			options = [ "${automount_opts},${credentials},${owner}" ];
+	sops.secrets = {
+		smbcreds = {
+			mode = "0600";
 		};
-		"/mnt/drive1" = {
-			device = "//10.0.10.2/drive1";
-			fsType = "cifs";
-			options = [ "${automount_opts},${credentials},${owner}" ];
-		};
-		"/mnt/drive2" = {
-			device = "//10.0.10.2/drive2";
-			fsType = "cifs";
-			options = [ "${automount_opts},${credentials},${owner}" ];
-		};
-		"/mnt/drive3" = {
-			device = "//10.0.10.2/drive3";
-			fsType = "cifs";
-			options = [ "${automount_opts},${credentials},${owner}" ];
+		davfs = {
+			mode = "0600";
+			path = "/etc/davfs2/secrets";
 		};
 	};
+
+	environment.systemPackages = with pkgs; [
+		cifs-utils
+		davfs2
+	];
+
+	services.davfs2.enable = true;
+
+	systemd.mounts = [
+		{
+			description = "NAS Drive 0";
+			after = [ "network-online.target" ];
+			wants = [ "network-online.target" ];
+			what = "//10.0.10.2/drive0";
+			where = "/mnt/drive0";
+			options = "credentials=${config.sops.secrets.smbcreds.path},${automount_opts},${owner}";
+			type = "cifs";
+		}
+		{
+			description = "NAS Drive 1";
+			after = [ "network-online.target" ];
+			wants = [ "network-online.target" ];
+			what = "//10.0.10.2/drive1";
+			where = "/mnt/drive1";
+			options = "credentials=${config.sops.secrets.smbcreds.path},${automount_opts},${owner}";
+			type = "cifs";
+		}
+		{
+			description = "NAS Drive 2";
+			after = [ "network-online.target" ];
+			wants = [ "network-online.target" ];
+			what = "//10.0.10.2/drive2";
+			where = "/mnt/drive2";
+			options = "credentials=${config.sops.secrets.smbcreds.path},${automount_opts},${owner}";
+			type = "cifs";
+		}
+		{
+			description = "NAS Drive 3";
+			after = [ "network-online.target" ];
+			wants = [ "network-online.target" ];
+			what = "//10.0.10.2/drive3";
+			where = "/mnt/drive3";
+			options = "credentials=${config.sops.secrets.smbcreds.path},${automount_opts},${owner}";
+			type = "cifs";
+		}
+		{
+			description = "Keepass WebDAV";
+			after = [ "network-online.target" ];
+			wants = [ "network-online.target" ];
+			what = "https://webdav.dungeon.melange.works";
+			where = "/mnt/webdav";
+			options = "${automount_opts},${owner}";
+			type = "davfs";
+			mountConfig.TimeoutSec = 15;
+		}
+	];
+
+	systemd.automounts = [
+		{	
+			where = "/mnt/drive0";
+			wantedBy = [ "multi-user.target" ];
+			automountConfig = {
+				TimeoutIdleSec = "120";
+			};
+		}
+		{	
+			where = "/mnt/drive1";
+			wantedBy = [ "multi-user.target" ];
+			automountConfig = {
+				TimeoutIdleSec = "120";
+			};
+		}
+		{	
+			where = "/mnt/drive2";
+			wantedBy = [ "multi-user.target" ];
+			automountConfig = {
+				TimeoutIdleSec = "120";
+			};
+		}
+		{	
+			where = "/mnt/drive3";
+			wantedBy = [ "multi-user.target" ];
+			automountConfig = {
+				TimeoutIdleSec = "120";
+			};
+		}
+		{	
+			where = "/mnt/webdav";
+			wantedBy = [ "multi-user.target" ];
+			automountConfig = {
+				TimeoutIdleSec = "120";
+			};
+		}
+	];
 }

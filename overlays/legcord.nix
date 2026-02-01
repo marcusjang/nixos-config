dbd6d97b4001532e63cc528a1b981eaa1968f68d
Marcus Jang <marcus@melange.works>
2026-02-01 23:41:06 +0900

    Up legcord to 1.2.1

--- overlays/legcord.nix
+++ overlays/legcord.nix
@@ -2,18 +2,18 @@
 final: prev: {
 	legcord = prev.legcord.overrideAttrs (finalAttrs: prevAttrs: rec {
 		pname = prevAttrs.pname;
-		version = "1.1.6";
+		version = "1.2.1";
 		src = final.pkgs.fetchFromGitHub {
 			owner = "Legcord";
 			repo = "Legcord";
-			#tag = "v${version}";
+			tag = "v${version}";
 			rev = "79bddf3d940b611d200ee4e808816583e801327e";
-			hash = "sha256-oYG33+s0VVboXjhuOfphAzbnUwwOSQmEIY1TopiQw5Q=";
+			hash = "";
 	  	};
 		pnpmDeps = final.pkgs.pnpm.fetchDeps {
 			inherit (finalAttrs) pname version src;
 			fetcherVersion = 1;
-			hash = "sha256-2zp+6wjTdbCp6VmN5d0NPir1VzSy6sOxkPbfFTvPXIM=";
+			hash = "";
 		};
 		autoPatchelfIgnoreMissingDeps = [ "*" ];
 	});

2cc27b9b4bc7f98dd98181a889221cc8b6e7db12
Marcus Jang <marcus@melange.works>
2026-02-05 03:32:23 +0900

    Revert fprintd driver to defaults

--- hosts/x1c13/default.nix
+++ hosts/x1c13/default.nix
@@ -13,8 +13,8 @@
 	boot.kernelPackages = pkgs.linuxPackages_latest;
 
 	services.fprintd.enable = true;
-	services.fprintd.tod.enable = true;
-	services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix-550a;
+	#services.fprintd.tod.enable = true;
+	#services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix-550a;
 
 	networking.hostName = "x1c13";
 	networking.networkmanager.enable = true;

0b7ca5cc96713584165efd5a9f98af3cf548d83f
Marcus Jang <marcus@melange.works>
2026-02-06 11:56:14 +0900

    Add configurationLimit to systemd-boot

--- hosts/x1c13/default.nix
+++ hosts/x1c13/default.nix
@@ -9,6 +9,7 @@
 
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.systemd-boot.consoleMode = "5";
+	boot.loader.systemd-boot.configurationLimit = 4;
 	boot.loader.efi.canTouchEfiVariables = true;
 	boot.kernelPackages = pkgs.linuxPackages_latest;
 

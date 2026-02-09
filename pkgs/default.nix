cf2f8f1d53ebeb7657e67cf9f9e78cc4d058e7fb
Marcus Jang <marcus@melange.works>
2026-02-09 09:59:40 +0900

    Sort packages by name

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,4 @@
 pkgs: with pkgs; {
-	gulim = callPackage ./gulim.nix { };
 	gnome-shell-power-off-options-ko = callPackage ./gnome-shell-power-off-options-ko.nix { };
+	gulim = callPackage ./gulim.nix { };
 }

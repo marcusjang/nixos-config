de89552a7fd840055b3be472b35e3694412c5fa4
Marcus Jang <marcus@melange.works>
2026-02-11 01:15:01 +0900

    Change indentations

--- modules/locale.nix
+++ modules/locale.nix
@@ -5,8 +5,10 @@
 		supportedLocales = [ "ko_KR.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
 	};
 	programs.dconf.profiles.user.databases = [{
-		settings."org/gnome/system/locale" = {
-			region = "ko_KR.UTF-8";
+		settings = {
+			"org/gnome/system/locale" = {
+				region = "ko_KR.UTF-8";
+			};
 		};
 	}];
 }

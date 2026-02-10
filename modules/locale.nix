ebaca133dc00c7832d966422567dc5b35b79eed9
Marcus Jang <marcus@melange.works>
2026-02-11 01:04:27 +0900

    Add gnome locale gsetting config

--- modules/locale.nix
+++ modules/locale.nix
@@ -1,7 +1,12 @@
 { ... }:
 {
-	 i18n = {
+	i18n = {
 		defaultLocale = "ko_KR.UTF-8";
 		supportedLocales = [ "ko_KR.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
 	};
+	programs.dconf.profiles.user.databases = [{
+		settings."org/gnome/system/locale" = {
+			region = "ko_KR.UTF-8";
+		};
+	}];
 }

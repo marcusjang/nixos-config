38497deb24aec9e57e231e8ba13310547b726aec
Marcus Jang <marcus@melange.works>
2026-02-15 23:46:33 +0900

    Enable fwupd

--- hosts/x1c13/default.nix
+++ hosts/x1c13/default.nix
@@ -16,6 +16,7 @@
 	services.fprintd.enable = true;
 	#services.fprintd.tod.enable = true;
 	#services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix-550a;
+	services.fwupd.enable = true;
 
 	networking.hostName = "x1c13";
 	networking.networkmanager.enable = true;

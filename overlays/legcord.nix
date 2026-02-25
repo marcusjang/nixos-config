f66abd464de4bfc1fddfb9ca14b0a0407315b902
Marcus Jang <marcus@melange.works>
2026-02-25 22:05:53 +0900

    Up legcord to 1.2.2

--- overlays/legcord.nix
+++ overlays/legcord.nix
@@ -2,17 +2,17 @@
 final: prev: {
 	legcord = prev.legcord.overrideAttrs (finalAttrs: prevAttrs: rec {
 		pname = prevAttrs.pname;
-		version = "1.2.1";
+		version = "1.2.2";
 		src = final.pkgs.fetchFromGitHub {
 			owner = "Legcord";
 			repo = "Legcord";
 			tag = "v${version}";
-			hash = "sha256-196AE244jEZNfhkC+vouNq9M7DOd3kk/1JLW1XRLOHA=";
-	  	};
+			hash = "sha256-i4Pw1jvkRYCQg1+9eZVi30Qblpttz9V+k//zehBZGDM=";
+		};
 		pnpmDeps = final.pkgs.fetchPnpmDeps {
 			inherit (finalAttrs) pname version src;
 			fetcherVersion = 1;
-			hash = "sha256-ksClxW8dIV72Hky5RFJ6cpPAteL29Cx8Me0aG5V/Y4k=";
+			hash = "sha256-9sdN5tbCCe/euTo8zRkU0C3yQ8sAufPyN8a4GeJW/Us=";
 		};
 		autoPatchelfIgnoreMissingDeps = [ "*" ];
 	});

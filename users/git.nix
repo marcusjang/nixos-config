ed70de50f5e23914304a6a5d2b064365eee23e4f
Marcus Jang <marcus@melange.works>
2026-03-03 23:26:09 +0900

    Update git home directory

--- users/git.nix
+++ users/git.nix
@@ -3,6 +3,7 @@
 	users.users.git = {
 		description = "GIT";
 		isNormalUser = true;
+		home = "/git";
 		extraGroups = [ ];
 		openssh.authorizedKeys.keys = config.users.users."marcus".openssh.authorizedKeys.keys;
 	};

2a4be63924cce7e950955ea52dbf1d4e10e3fc99
Marcus Jang <marcus@melange.works>
2026-03-04 19:08:10 +0900

    Add preliminary homebridge plugin configs

--- modules/server/homebridge.nix
+++ modules/server/homebridge.nix
@@ -1,5 +1,6 @@
-{ config, ... }:
-{
+{ config, lib, pkgs, ... }: let
+	marcus-desktop = "10.0.10.10";
+in {
 	sops.secrets = {
 		"homebridge/sshKey" = {
 			mode = "0400";
@@ -16,6 +17,31 @@
 	services.homebridge = {
 		enable = true;
 		openFirewall = true;
+		settings = {
+			accessories = [
+				{
+					accessory = "NetworkDevice";
+					model = "NetworkDevice";
+					name = "marcus@desktop";
+					host = marcus-desktop;
+					broadcastAddress = "10.0.10.255";
+					mac = "34:5A:60:15:20:B1";
+					pingCommand = "${lib.getExe' pkgs.iputils "ping"} -c 1 ${marcus-desktop}";
+					pingCommandTimeout = 1;
+					pingTimeout = 1;
+					pingInterval = 2;
+					pingsToChange = 5;
+					startCommandTimeout = 0;
+					wakeGraceTime = 30;
+					wakeCommandTimeout = 0;
+					shutdownCommand = "${lib.getExe pkgs.openssh} -o StrictHostKeyChecking=no -i ${config.sops.secrets."homebridge/sshKey".path} marcus@${marcus-desktop} \"shutdown /h\"";
+					shutdownGraceTime = 15;
+					shutdownCommandTimeout = 5;
+					logLevel = "Info";
+					returnEarly = false;
+				}
+			];
+		};
 	};
 
 	services.traefik.dynamicConfigOptions.http = {

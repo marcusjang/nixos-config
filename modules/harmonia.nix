bd58f10de8c69f7f1dea7764a60de6551b0b0093
Marcus Jang <marcus@melange.works>
2026-04-13 03:37:37 +0900

    Remove unnecessary code

--- modules/harmonia.nix
+++ modules/harmonia.nix
@@ -1,5 +1,4 @@
 {
-	sops.secrets."harmonia/pub".mode = "0400";
 	nix.settings = {
 		substituters = [ "https://nix-cache.dungeon.melange.works" ];
 		trusted-public-keys = [ "nix-cache.dungeon.melange.works-1:JlaHA9DZ4qyt2nggs45O39i+C7hVtrcOxaex63rZghg=" ];

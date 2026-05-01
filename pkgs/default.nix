82101fb981147e51f92d967922bab571b122f200
Marcus Jang <marcus@melange.works>
2026-05-01 20:05:16 +0900

    Add birdtray

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,5 @@
 pkgs: with pkgs; {
+	birdtray = callPackage ./birdtray.nix { };
 	deno-bin = callPackage ./deno-bin.nix { };
 	gulim = callPackage ./gulim.nix { };
 	hop = callPackage ./hop.nix { };

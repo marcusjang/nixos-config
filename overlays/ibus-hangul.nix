a3207d8f7df52191a6caf3add00131902d22d0f7
Marcus Jang <marcus@melange.works>
2026-05-03 23:40:41 +0900

    Patch ibus-hangul for simplified input mode symbols

--- overlays/ibus-hangul.nix
+++ overlays/ibus-hangul.nix
@@ -8,5 +8,6 @@ final: prev: with final; {
 			rev = "9ee4419ebca803a4f7f2d9c31550abb2c8017de0";
 			hash = "sha256-eBnwOlo8g2aaATZHB8RqPxsHwWz8KVSjf1bBwl9zO1Q=";
 		};
+		patches = [ ./patches/ibus-hangul.patch ];
 	});
 }

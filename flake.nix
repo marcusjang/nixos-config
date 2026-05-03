affbdaaef2b70101c4d0797bd922ac2ba526e557
Marcus Jang <marcus@melange.works>
2026-05-04 03:52:55 +0900

    Up harmonia to 3.1.0

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 		nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 		hardware.url = "github:nixos/nixos-hardware";
 		wsl.url = "github:nix-community/NixOS-WSL/main";
-		harmonia.url = "github:nix-community/harmonia?ref=0327e9d607d42ccafaad2f1b24fabec8ab99fd9f";
+		harmonia.url = "github:nix-community/harmonia?ref=harmonia-v3.1.0";
 		flake-utils.url = "github:numtide/flake-utils";
 		copyparty.url = "github:9001/copyparty";
 		niri.url = "github:sodiboo/niri-flake";

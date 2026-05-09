6e161c8984e7d1416b29dddc5f9ba2b5af5f8113
Marcus Jang <marcus@melange.works>
2026-05-10 02:11:49 +0900

    Add ibus related dconf settings

--- modules/desktop/ime/ibus.nix
+++ modules/desktop/ime/ibus.nix
@@ -14,4 +14,18 @@
 	};
 
 	environment.variables."GTK_IM_MODULE" = lib.mkForce "";
+	environment.sessionVariables."IBUS_USE_PORTAL" = "1";
+
+	programs.dconf = {
+		profiles.user.databases = [{
+			settings = {
+				"desktop/ibus/engine/hangul" = {
+					"word-commit" = false;
+					"use-system-keyboard-layout" = true;
+					"hanja-keys" = "Hangul_Hanja";
+					"initial-input-mode" = "latin";
+				};
+			};
+		}];
+	};
 }

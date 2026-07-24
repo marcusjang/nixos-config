5909f83cade3034626153a78f6a2c2a972963cb8
Marcus Jang <marcus@melange.works>
2026-07-24 23:05:54 +0900

    Up tls-client to nightly

--- pkgs/tls-client.nix
+++ pkgs/tls-client.nix
@@ -5,16 +5,17 @@
 }:
 buildGoModule (finalAttrs: {
 	pname = "tls-client";
-	version = "1.15.1";
+	version = "1.15.1-nightly";
 
 	src = fetchFromGitHub {
 		owner = "bogdanfinn";
 		repo = "tls-client";
-		tag = "v${finalAttrs.version}";
-		hash = "sha256-m64nDlPwRWNEfb4dQV6gxwhe80/zcgnfruhQkZoShqc=";
+		#tag = "v${finalAttrs.version}";
+		rev = "b790a311273f26051935641120de169e497e5943";
+		hash = "sha256-qYm48+wjdvZ21lAjAMGPKGVuA1K50sTnPM09+MK44BA=";
 	};
 
-	vendorHash = "sha256-CifciilXj7tFKbHSux+mKnLAWYxBqBWS1ngIo6V92Nw=";
+	vendorHash = "sha256-ZvH9Ndnt5u/uH+4Q95rt54wN6yzKTOx9zld38e7Zg3E=";
 
 	modRoot = "./cffi_dist";
 	buildPhase = "go build -buildmode=c-shared -o $out/lib/tls-client.so";
@@ -27,6 +28,7 @@ buildGoModule (finalAttrs: {
 		maintainers = [ lib.importJSON ../maintainer.json ];
 		platforms = [
 			"x86_64-linux"
+			"aarch64-linux"
 		];
 	};
 })

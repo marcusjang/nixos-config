48d050d1724176785a7a1b1403676aa642144563
Marcus Jang <marcus@melange.works>
2026-08-08 11:34:33 +0900

    Rewrite kime configuration with `inputMethod.enable = false`

--- modules/desktop/ime/kime.nix
+++ modules/desktop/ime/kime.nix
@@ -1,19 +1,46 @@
-{ lib, pkgs, ... }:
-{
-	i18n.inputMethod = {
-		enable = true;
-		type = "ibus";
-		ibus.engines = [
-			pkgs.kime
-			pkgs.ibus-engines.hangul # fallback for unstable kime-ibus
+{ lib, pkgs, config, ... }: let
+	ibusPackage = pkgs.ibus-with-plugins.override {
+		plugins = with pkgs; [
+			kime
+			ibus-engines.hangul
 		];
 	};
+in {
+	i18n.inputMethod.enable = false;
 
-	environment.systemPackages = [ pkgs.kime ];
+	environment.systemPackages = [
+		ibusPackage
+		pkgs.kime
+	];
+
+    environment.etc."xdg/autostart/ibus-daemon.desktop".text = ''
+        [Desktop Entry]
+        Name=Ibus
+        Type=Application
+        Exec=${ibusPackage}/bin/ibus-daemon --daemonize --xim
+        NotShowIn=GNOME;KDE;
+    '';
+
+    environment.etc."xdg/kime/config.yaml".text = ''
+        daemon:
+          modules: 
+          - Xim
+          - Wayland
+          - Indicator
+        indicator:
+          icon_color: White
+    '';
 
 	environment.variables = {
-		GTK_IM_MODULE = lib.mkForce "kime";
-		QT_IM_MODULE = lib.mkForce "kime";
-		XMODIFIERS = lib.mkForce "@im=kime";
+		GTK_IM_MODULE = "kime";
+		QT_IM_MODULE = "kime";
+		XMODIFIERS = "@im=kime";
 	};
+
+	programs.dconf.enable = true;
+	programs.dconf.packages = [ ibusPackage ];
+
+	services.dbus.packages = [ ibusPackage ];
+
+	xdg.portal.extraPortals = lib.mkIf config.xdg.portal.enable [ ibusPackage ];
 }
